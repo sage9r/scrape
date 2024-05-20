ca0cb85721584023be7471ff81e46772123bfe3a
sagarkafle <sagarkafle39@gmail.com>
2024-05-20 21:47:10 +0545

    database settings postgres_+ supabase

--- scrape/settings.py
+++ scrape/settings.py
@@ -80,11 +80,10 @@ WSGI_APPLICATION = 'scrape.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get("DB_NAME"),
-        'USER': os.environ.get("DB_USER"),
-        'PASSWORD': os.environ.get("DB_PASSWORD"),
-        'HOST': os.environ.get("DB_HOST"),
-        'PORT': os.environ.get("DB_PORT"),
+        'NAME': os.environ.get("POSTGRES_DATABASE"),
+        'USER': os.environ.get("POSTGRES_USER"),
+        'PASSWORD': os.environ.get("POSTGRES_PASSWORD"),
+        'HOST': os.environ.get("POSTGRES_HOST"),
     }
 }
 
