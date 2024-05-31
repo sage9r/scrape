d602fbe1685080e9c99d9b9c381722f7da5bcb9d
sagarkafle <sagarkafle39@gmail.com>
2024-05-31 12:54:36 +0545

    email field register form

--- users/views.py
+++ users/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render
 from django.contrib.auth.models import User
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
-from django.views.generic import CreateView, FormView
+from django.views.generic import CreateView
+from .forms import UserRegisterForm
 
 # Create your views here.
 class UserRegisterView(CreateView):
     model = User
-    form_class = UserCreationForm
+    form_class = UserRegisterForm
     success_url = reverse_lazy('homes')
 
 
