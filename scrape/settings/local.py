49751b3505e201548b364ce480c7331d49c6fa3a
sagarkafle <sagarkafle39@gmail.com>
2024-09-10 23:52:49 +0545

    removed local from gitignore

---
+++ scrape/settings/local.py
@@ -0,0 +1,28 @@
+from .settings import *
+
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'livereload',
+    'django.contrib.staticfiles',
+    'home',
+    'users',
+    "crispy_bootstrap5",
+    'crispy_forms'
+]
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'livereload.middleware.LiveReloadScript',
+]
+
