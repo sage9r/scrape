36eba2240d2f68e8ba65720d16a1c50c542ac050
sagarkafle <sagarkafle39@gmail.com>
2024-09-14 22:46:13 +0545

    staticfiles_dirs

--- scrape/settings/settings.py
+++ scrape/settings/settings.py
@@ -134,6 +134,7 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
