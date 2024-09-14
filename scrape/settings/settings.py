4ac73358935636c49bffbccd566afb535ff82c8d
sagarkafle <sagarkafle39@gmail.com>
2024-09-14 21:43:09 +0545

    updated vercel.json for staticfiles

--- scrape/settings/settings.py
+++ scrape/settings/settings.py
@@ -134,8 +134,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'expenseswebsite', 'static'),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = 'bootstrap5'
 CRISPY_TEMPLATE_PACK = 'bootstrap5'
